006cd1ee716cb862aeb63fdd6cf0e8a35d99c364
kitsana phomgarm <nick7266@hotmail.com>
2018-12-25 16:28:21 +0700

    Complete workshop703

---
+++ workshop703/workshop703/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim word As String = TextBox1.Text.ToLower
+        Dim Tword As String = TextBox1.Text
+        Dim listVowel() As String = {"a", "e", "i", "o", "u"}
+        Dim vowel As String = ""
+        Dim alphabet As String = ""
+
+        For i = 0 To (word.Length - 1)
+            Dim check As Boolean = True
+            For Each item In listVowel
+                If word(i) = item Then
+                    vowel &= $"{Tword(i)} "
+                    check = False
+                    Exit For
+                End If
+            Next
+            If check = True Then
+                alphabet &= $"{Tword(i)} "
+            End If
+        Next
+
+        TextBox2.Text = vowel
+        TextBox3.Text = alphabet
+    End Sub
+End Class
