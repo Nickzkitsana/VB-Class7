cc51c3a900413d46a5f5aa0efec29064425df767
kitsana phomgarm <nick7266@hotmail.com>
2018-12-25 15:51:37 +0700

    Complete workshop701

---
+++ workshop701/workshop701/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim CZY() As String = {"ชวด", "ฉลู", "ขาล", "เถาะ", "มะโรง", "มะเส็ง", "มะเมีย", "มะแม", "วอก", "ระกา", "จอ", "กุน"}
+
+        Dim year As Integer = TextBox1.Text
+        Dim calczy = (year + 5) Mod 12
+
+        MessageBox.Show($"พ.ศ. {year} ตรงกับปี: {CZY(calczy)}")
+    End Sub
+End Class
